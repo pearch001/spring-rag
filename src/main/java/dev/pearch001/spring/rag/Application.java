c57de2e6ef24638435d327128e42aeb2e76d56ab
pearch001 <kasimoluwasegun@gmail.com>
2025-04-01 19:22:13 +0100

    first commit

---
+++ src/main/java/dev/pearch001/spring/rag/Application.java
@@ -0,0 +1,13 @@
+package dev.pearch001.spring.rag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/dev/pearch001/spring/rag/ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.pearch001.spring.rag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
